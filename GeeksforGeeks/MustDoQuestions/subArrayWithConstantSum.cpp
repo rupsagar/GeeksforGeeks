acfd13b3d7682f8effaba572be2fdcc264fd3509
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2021-07-03 16:50:03 +0530

    Add files via upload

---
+++ GeeksforGeeks/MustDoQuestions/subArrayWithConstantSum.cpp
@@ -0,0 +1,58 @@
+//Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+//Driver Code Ends
+
+//Solution Class Starts
+class Solution{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, int s){
+        //Your code here
+        vector<int> pos={1};
+        int sum=0;
+        if (s>0){
+            for (int i=0;i<n;++i){
+                sum=sum+arr[i];
+                while(sum>s){
+                    sum=sum-arr[pos[0]-1];
+                    pos[0]=pos[0]+1;
+                }
+                if(sum==s){
+                    pos.push_back(i+1);
+                    break;
+                }
+            }     
+        }
+        if (pos.size()==1)
+            pos.front()=-1;
+
+        return pos;
+    }
+};
+//Solution Class Ends
+
+//Driver Code Starts
+int main(){
+    int t;
+    cout<<"Enter the number of runs: ";
+    cin>>t;
+    while(t--){
+        int n;
+        long long s;
+        cout<<"Enter the size of array and desired sum: ";
+        cin>>n>>s;
+        int arr[n];
+        cout<<"Enter the elements of array: ";
+        for(int i=0;i<n;++i)
+            cin>>arr[i];
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        cout<<"The required subarray is from: ";
+        for(int i = 0;i<res.size();++i)
+            cout<<res[i]<<"\t";        
+    }
+	return 0;
+}
+//Driver Code Ends
